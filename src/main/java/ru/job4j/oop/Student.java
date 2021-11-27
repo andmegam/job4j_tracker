9c15b7f071627bd47dc8c71887ed56cf169ac0c9
andmmm <andmmm@mail.ru>
2021-11-27 15:15:20 +0400

    1.2. Вызов метода объекта [#166085] fix #1

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop;
 
 public class Student {
+
     public static void main(String[] args) {
         Student petya = new Student();
         petya.music();
@@ -10,10 +11,13 @@ public class Student {
         petya.song();
         petya.song();
     }
+
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public void song() {
         System.out.println("I believe I can fly");
     }
+
 }
