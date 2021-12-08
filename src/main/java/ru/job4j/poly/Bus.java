d7eebc58c962fd2ee43de4c4cf7123789b73ca8e
andmmm <andmmm@mail.ru>
2021-12-08 20:54:34 +0400

    2.8. Реализация класса StartUI. Вывод заявок по имени [#500749]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,31 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    private int petrolLiter;
+    private int countPass;
+
+    public Bus(int countPass, int petrolLiter) {
+        this.countPass = countPass;
+        this.petrolLiter = petrolLiter;
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("В автобусе " + countPass + " пассажиров, "
+                + "кол-во топлива: " + this.petrolLiter
+                + "Сумма топлива: " + refuel(this.petrolLiter)
+        );
+    }
+
+    @Override
+    public void passengers(int countPass) {
+        System.out.println("В рейс отправляется " + countPass + " пассажиров.");
+    }
+
+    @Override
+    public double refuel(int petrolLiter) {
+        double oilPrice = 49.56;
+        return petrolLiter * oilPrice;
+    }
+}
