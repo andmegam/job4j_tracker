e417cc5219aa63f7a3dafb1390c6fb6cdda5be83
andmmm <andmmm@mail.ru>
2022-07-27 19:50:16 +0400

    3. Модели данных и HashSet. [#218530 #326210]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class NotifyAccountTest {
@@ -19,6 +20,25 @@ class NotifyAccountTest {
                 Arrays.asList(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts)).containsExactlyElementsOf(expect);
+    }
+
+    @Test
+    public void sentWhichTwoDuplicate() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Александр Пушкин", "123000"),
+                new Account("456", "Иван Бунин", "456000"),
+                new Account("789", "Афанасий Фет", "789000"),
+                new Account("123", "Александр Пушкин", "123000"),
+                new Account("789", "Афанасий Фет", "789000")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("789", "Афанасий Фет", "789000"),
+                        new Account("456", "Иван Бунин", "456000"),
+                        new Account("123", "Александр Пушкин", "123000")
                 )
         );
         assertThat(NotifyAccount.sent(accounts)).containsExactlyElementsOf(expect);
