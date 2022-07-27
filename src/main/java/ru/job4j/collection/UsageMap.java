64ab47a5e02c3f692c66804e12e2277d0850a34a
andmmm <andmmm@mail.ru>
2022-07-27 20:13:17 +0400

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("pushkin_a@mail.ru", "Александр Пушкин");
+        map.put("bunin_i@yandeex.ru", "Иван Бунин");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
